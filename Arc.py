cd7abf59d777ecd6971005c9f08ea797ebe563b5
Shramana-23 <63316393+Shramana-23@users.noreply.github.com>
2020-07-02 10:27:39 +0530

    Update Arc.py
    1.Area of circle
    2.Filename extension

--- Arc.py
+++ Arc.py
@@ -1,6 +1,10 @@
-# Mycaptain
+# Mycaptain """Area of circle"""
 import math
 r=float(input("Enter the radius of circle:"))
 area=math.pi*(r* r)
 print("The area of the circle with radius ",r," is:",area)
 
+"""Filename extension"""
+filename=input("Enter a filename")
+ext=filename.split(".")
+print("Extension of the file ",filename," is ",ext)
